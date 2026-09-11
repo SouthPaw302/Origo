cd429a8c00db5a0e1af10aaa550acfa65e4d4335
SouthPaw302 <103312472+SouthPaw302@users.noreply.github.com>
2026-09-11 02:12:53 -0400

    Install interaction pacing before world pacing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {installAudioGovernor} from './audio/audioGovernor';
+import {installInteractionPacing} from './simulation/interactionPacing';
 import {installWorldPacing} from './simulation/worldPacing';
 import './index.css';
 
+installInteractionPacing();
 installWorldPacing();
 installAudioGovernor();
 
